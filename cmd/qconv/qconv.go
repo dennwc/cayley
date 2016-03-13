d6da34e5d78f0c453eff62afa54231861f9401ac
Denys Smirnov <denis.smirnov.91@gmail.com>
2016-03-13 15:14:20 +0200

    Additional stats for written data. Allow to bypass writing.

--- cmd/qconv/qconv.go
+++ cmd/qconv/qconv.go
@@ -32,17 +32,46 @@ func (w writeHook) WriteQuad(q quad.Quad) error {
 	return nil
 }
 
+type devNull struct{}
+
+func (w devNull) WriteQuad(q quad.Quad) error { return nil }
+func (w devNull) Close() error                { return nil }
+
+type quadHook struct {
+	w quad.WriteCloser
+	f []func(q quad.Quad)
+}
+
+func (w quadHook) WriteQuad(q quad.Quad) error {
+	for _, f := range w.f {
+		f(q)
+	}
+	return w.w.WriteQuad(q)
+}
+func (w quadHook) Close() error { return w.w.Close() }
+
+var (
+	f_drop  = flag.Bool("drop", false, "do not write quads to output, read providen files only")
+	f_types = flag.Bool("types", false, "print all basic types found in dataset")
+	f_lang  = flag.Bool("lang", false, "print all languages found in dataset")
+)
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	if len(args) < 2 {
+	if (!*f_drop && len(args) < 2) || (*f_drop && len(args) < 1) {
 		fmt.Println("usage: qconv <src> <dst>")
 		os.Exit(1)
 	}
 	start := time.Now()
-	var qw quad.WriteCloser
 	var cnt int
-	{
+	defer func() {
+		log.Printf("written %d quads in %v", cnt, time.Since(start))
+	}()
+	var qw quad.WriteCloser
+	if *f_drop {
+		qw = devNull{}
+	} else {
 		name := args[len(args)-1]
 		args = args[:len(args)-1]
 		file, err := os.Create(name)
@@ -65,8 +94,55 @@ func main() {
 		qw = f.Writer(w)
 	}
 	defer qw.Close()
+
+	var qh []func(q quad.Quad)
+	if *f_types {
+		m := make(map[quad.IRI]struct{})
+		check := func(v quad.Value) {
+			if t, ok := v.(quad.TypedString); ok {
+				if _, ok = m[t.Type]; !ok {
+					log.Println("new type found:", t.Type)
+					m[t.Type] = struct{}{}
+				}
+			}
+		}
+		defer func() {
+			log.Printf("dataset uses %d basic types:\n%v", len(m), m)
+		}()
+		qh = append(qh, func(q quad.Quad) {
+			check(q.Subject)
+			check(q.Predicate)
+			check(q.Object)
+		})
+	}
+	if *f_lang {
+		m := make(map[string]struct{})
+		check := func(v quad.Value) {
+			if t, ok := v.(quad.LangString); ok {
+				if _, ok = m[t.Lang]; !ok {
+					log.Println("new language found:", t.Lang)
+					m[t.Lang] = struct{}{}
+				}
+			}
+		}
+		defer func() {
+			log.Printf("dataset uses %d languages:\n%v", len(m), m)
+		}()
+		qh = append(qh, func(q quad.Quad) {
+			check(q.Subject)
+			check(q.Predicate)
+			check(q.Object)
+		})
+	}
+	if qh != nil {
+		qw = quadHook{w: qw, f: qh}
+	}
+
 	var errored bool
 	for _, name := range args {
+		if name == "" || name == "-" {
+			continue
+		}
 		err := func() error {
 			var qr quad.ReadCloser
 			file, err := os.Open(name)
@@ -106,7 +182,6 @@ func main() {
 			continue
 		}
 	}
-	log.Printf("written %d quads in %v", cnt, time.Since(start))
 	if errored {
 		os.Exit(1)
 	}
