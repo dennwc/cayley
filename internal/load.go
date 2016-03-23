001e65c8b2a8e67e7a09f63127fffd1f3ddf7400
Denys Smirnov <denis.smirnov.91@gmail.com>
2016-03-23 22:38:05 +0200

    Load pquads in cayley binary

--- internal/load.go
+++ internal/load.go
@@ -14,6 +14,7 @@ import (
 	"github.com/google/cayley/quad"
 	"github.com/google/cayley/quad/cquads"
 	"github.com/google/cayley/quad/nquads"
+	_ "github.com/google/cayley/quad/pquads"
 
 	// Register other supported decoding formats
 	_ "github.com/google/cayley/quad/json"
