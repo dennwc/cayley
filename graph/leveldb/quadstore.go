945a4a80b05a8fc3ed0eef0268065c8754011a37
dennwc <denis.smirnov.91@gmail.com>
2016-03-24 16:47:28 +0200

    Do not log failed quad add/remove attempts

--- graph/leveldb/quadstore.go
+++ graph/leveldb/quadstore.go
@@ -306,11 +306,15 @@ func (qs *QuadStore) buildQuadWrite(batch *leveldb.Batch, q quad.Quad, id int64,
 	}
 
 	if isAdd && len(entry.History)%2 == 1 {
-		glog.Errorf("attempt to add existing quad %v: %#v", entry, q)
+		if glog.V(2) {
+			glog.Errorf("attempt to add existing quad %v: %#v", entry, q)
+		}
 		return graph.ErrQuadExists
 	}
 	if !isAdd && len(entry.History)%2 == 0 {
-		glog.Errorf("attempt to delete non-existent quad %v: %#c", entry, q)
+		if glog.V(2) {
+			glog.Errorf("attempt to delete non-existent quad %v: %#c", entry, q)
+		}
 		return graph.ErrQuadNotExist
 	}
 
